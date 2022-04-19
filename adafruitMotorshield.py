e66ed9860f9a5e4edb8b1de3fc2b2f0702d5af20
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-04-19 15:17:41 +0100

    adafruitMotorshield.py programmed

--- adafruitMotorshield.py
+++ adafruitMotorshield.py
@@ -1,15 +1,76 @@
 import time
 import board
 from adafruit_motorkit import MotorKit
-
 kit = MotorKit(i2c=board.I2C())
 
-kit.motor1.throttle = 1.0
-time.sleep(0.5)
-kit.motor1.throttle = 0
+class AdafruitDCMotor:
+    motorSpeed=1.0
+    def __init__(self):
+        #Uses the first motor of the motor kit as a default
+        self.dcMotor=self.kit.motor1
 
-"""Simple test for using adafruit_motorkit with a stepper motor"""
+        self.dcMotor.throttle = 0
 
-for i in range(100):
-    kit.stepper1.onestep()
-    time.sleep(0.01)
+
+    #Holds the motor in a certain position
+    def stop(self):
+        self.dcMotor.throttle = 0
+
+    #Max Speed is 1
+    def setMotorDCSpeed(self,motorSpeed):
+        self.motorSpeed=motorSpeed
+
+    def forward(self):
+        self.dcMotor.throttle = self.motorSpeed
+
+    def backward(self):
+        self.dcMotor.throttle=-self.motorSpeed
+
+    #The motor can be moved without any resistance
+    def release(self):
+        self.dcMotor.throttle= None
+
+
+
+    """Using adafruit_motorkit with a stepper motor"""
+class AdafruitStepper:
+
+    distanceTraveled=0
+    stepsMade = 0.0
+    def __init__(self,distanceOneStep,distanceOneMicrostep):
+        #This Attribute needs to be calculate with the size of the Wheel
+        self.distanceOneStep=distanceOneStep
+        #Attributes is the result of distanceOneStep by 8
+        self.distanceOneMicrostep=distanceOneMicrostep
+
+        self.stepperMotor=self.kit.stepper1
+        self.stepperMotor.release()
+        self.stepperMotorStyle=self.stepper.SINGLE
+
+    def oneStepForward(self):
+        self.stepperMotor.onestep(direction=self.stepper.FORWARD,style=self.stepperMotorStyle)
+        self.distanceTraveled+=self.distanceOneStep
+        self.stepsMade+=1
+
+    def oneStepBackward(self):
+        self.stepperMotor.onestep(direction=self.stepper.BACKWARD,style=self.stepperMotorStyle)
+        self.distanceTraveled-=self.distanceOneStep
+        self.stepsMade+=1
+
+    def oneMicroStepForward(self):
+        self.stepperMotor.onestep(direction=self.stepper.FORWARD,style=self.MICROSTEP)
+        self.distanceTraveled+=self.distanceOneMicrostep
+        self.stepsMade+=0.125
+
+    def oneMicroStepBackward(self):
+        self.stepperMotor.onestep(direction=self.stepper.BACKWARD,style=self.MICROSTEP)
+        self.distanceTraveled-=self.distanceOneMicrostep
+        self.stepsMade+=0.125
+
+    def resetDistanceTraveled(self):
+        self.distanceTraveled=0
+
+    def resetDistanceTraveled(self):
+        self.stepsMade=0
+
+
