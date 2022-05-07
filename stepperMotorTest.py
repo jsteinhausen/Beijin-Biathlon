a2d9440d80215a1b5b1549b7d70ad5c3977a64b0
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-07 18:44:50 +0200

    adafruitMotorshield.py stepper motor configured, method distance2steps() added, stepperMotorTest.py added stepper class imported from adafruit_motor

--- stepperMotorTest.py
+++ stepperMotorTest.py
@@ -4,7 +4,8 @@ import time
 shield=adafruitMotorshield.AdafruitMotorShield()
 shield.createStepperMotor(1.8,6.0,0.1)
 
-for i in range(shield.adafruitStepperMotor.distance2steps(100)):
+#for i in range(shield.adafruitStepperMotor.distance2steps(100)):
+while True:
     shield.adafruitStepperMotor.oneStepForward()
     time.sleep(0.01)
 
