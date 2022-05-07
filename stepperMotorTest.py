eb79c085f3d36182650d4e9a6ca210a2befb48e0
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-07 18:52:46 +0200

    stepperMotorTest.py stepperspeed test 0.001s sleep

--- stepperMotorTest.py
+++ stepperMotorTest.py
@@ -7,8 +7,9 @@ shield.createStepperMotor(1.8,6.0,0.1)
 #for i in range(shield.adafruitStepperMotor.distance2steps(100)):
 while True:
     shield.adafruitStepperMotor.oneStepForward()
+    #0.001 is perfect
     #0.0009 is stable
-    #0.0008
+    #0.0008 is stable
     time.sleep(0.0008)
 
 shield.adafruitStepperMotor.stepperMotor.release()
