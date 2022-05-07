ee8e76cbd8e14d6c3e608cbdafcd865a9575a0aa
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-07 18:46:26 +0200

    stepperMotorTest.py stepperspeed test

--- stepperMotorTest.py
+++ stepperMotorTest.py
@@ -7,7 +7,7 @@ shield.createStepperMotor(1.8,6.0,0.1)
 #for i in range(shield.adafruitStepperMotor.distance2steps(100)):
 while True:
     shield.adafruitStepperMotor.oneStepForward()
-    time.sleep(0.01)
+    time.sleep(0.001)
 
 shield.adafruitStepperMotor.stepperMotor.release()
 print(shield.adafruitStepperMotor.stepsMade)
