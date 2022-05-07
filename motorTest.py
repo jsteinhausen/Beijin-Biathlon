271c1f7875d39fd2c85201f5d71f37e806337ba5
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-07 12:25:59 +0200

    adafruitMotorshield.py test uebergabe

--- motorTest.py
+++ motorTest.py
@@ -4,6 +4,6 @@ import time
 shield=adafruitMotorshield.AdafruitMotorShield()
 shield.createDCMotor()
 
-shield.adafruitDCMotor.dcMotor.forward()
+shield.adafruitDCMotor.forward()
 time.sleep(2)
-shield.adafruitDCMotor.dcMotor.stop()
+shield.adafruitDCMotor.stop()
