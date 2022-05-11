a6f76c16b26d9b65d595fe9d0977fb8672986c06
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-11 17:04:18 +0200

    movetest 1

--- stepperMotorTest.py
+++ stepperMotorTest.py
@@ -2,7 +2,7 @@ import adafruitMotorshield
 import time
 
 shield=adafruitMotorshield.AdafruitMotorShield()
-shield.createStepperMotor(1.8,6.0,0.1)
+shield.createStepperMotor(1.8,6.5,0.1)
 
 #for i in range(shield.adafruitStepperMotor.distance2steps(100)):
 
