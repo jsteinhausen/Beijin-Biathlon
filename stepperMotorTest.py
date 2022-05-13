3e467817bd3cd983f6e11dbe20b3696e7a9c68ed
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-13 15:17:48 +0200

    stepperMotorTest.py test 0.006

--- stepperMotorTest.py
+++ stepperMotorTest.py
@@ -3,7 +3,7 @@ import time
 
 shield=adafruitMotorshield.AdafruitMotorShield()
 shield.createStepperMotor(1.8,65,0.1)
-shield.adafruitStepperMotor.stepperSpeed=0.005
+shield.adafruitStepperMotor.stepperSpeed=0.006
 
 #for i in range(shield.adafruitStepperMotor.distance2steps(100)):
 
