fee8e140e4b907018b7e37d7171ca662a74979d7
Fabian Schenker <fabian.schenker5301@gmail.com>
2022-05-15 18:01:55 +0200

    commit ServoTestClass.py

---
+++ ServoTestClass.py
@@ -0,0 +1,41 @@
+# import RPi.GPIO as GPIO
+# import time
+
+servoPIN = 12
+
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(servoPIN, GPIO.OUT)
+
+# p = GPIO.PWM(servoPIN, 50)  # GPIO 12 for PWM with 50Hz
+# p.start(2.5)  # Initialization
+
+# ChangeDutyCycle(5) 5 steht fuer pulsweite von 5% von 20ms (f=50Hz)
+# 5% -> 0°
+# 10% -> 90° (maxwert von verwendetem servo)
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+
+angle = 45
+pwnPercent = translate(angle, 0.0, 45.0, 5.0, 10.0)
+print(pwnPercent)
+
+
+try:
+    while True:
+        p.ChangeDutyCycle(pwnPercent)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
+
