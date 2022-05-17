e3ff27a2ae4aa64ff1447e3993ba15f4cf19461b
Johannes <johannes.steinhausen@gmail.com>
2022-05-17 17:57:16 +0200

    resolve merge conflict

--- stepperMotorTest.py
+++ stepperMotorTest.py
@@ -3,11 +3,7 @@ import time
 
 shield=adafruitMotorshield.AdafruitMotorShield()
 shield.createStepperMotor(1.8,65,0.1)
-<<<<<<< HEAD
-shield.adafruitStepperMotor.stepperSpeed=0.004
-=======
 shield.adafruitStepperMotor.stepperSpeed=0.005
->>>>>>> cd541c88788989155440ba83b4d80a83f46c381b
 
 #for i in range(shield.adafruitStepperMotor.distance2steps(100)):
 
