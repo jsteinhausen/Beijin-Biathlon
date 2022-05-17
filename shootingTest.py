ce07f0796eee23074705e2695ee2a6daca745436
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-17 19:29:15 +0200

    shootingTest.py Gpio test

--- shootingTest.py
+++ shootingTest.py
@@ -5,7 +5,7 @@ import adafruitMotorshield
 shield=adafruitMotorshield.AdafruitMotorShield()
 shield.createDCMotor()
 switch=12
-GPIO.setup(switch, GPIO.IN)
+GPIO.setup(switch, GPIO.IN, GPIO.PUD_OFF)
 while True:
     print(GPIO.input(switch))
 #while not GPIO.input(switch):
