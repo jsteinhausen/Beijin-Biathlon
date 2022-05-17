ebccabf5e4a3790e3391b85aa54532a2d459937a
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-17 18:43:33 +0200

    shootingTest.py with motor

--- shootingTest.py
+++ shootingTest.py
@@ -1,8 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+shield=adafruitMotorshield.AdafruitMotorShield()
+shield.createDCMotor()
 GPIO.setmode(GPIO.BOARD)
 switch=12
 GPIO.setup(switch, GPIO.IN)
 
-while True:
-    print(GPIO.input(switch))
+while GPIO.input(switch):
+    shield.adafruitDCMotor.backward()
+
+shield.adafruitDCMotor.stop()
