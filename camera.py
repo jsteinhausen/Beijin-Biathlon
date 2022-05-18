20ef13785ed678f01593e0aa7e8618501e1a8536
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-18 16:10:45 +0200

    camera.py added

--- camera.py
+++ camera.py
@@ -1,20 +1,10 @@
 import cv2
-import sys
 
-s = 0
-if len(sys.argv) > 1:
-    s = sys.argv[1]
+cap = cv2.VideoCapture(0)
 
-source = cv2.VideoCapture(s)
+# Capture frame
+ret, frame = cap.read()
+if ret:
+	cv2.imwrite('image.jpg', frame)
 
-win_name = 'Camera Preview'
-cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-
-while cv2.waitKey(1) != 27: # Escape
-    has_frame, frame = source.read()
-    if not has_frame:
-        break
-    cv2.imshow(win_name, frame)
-
-source.release()
-cv2.destroyWindow(win_name)
+cap.release()
