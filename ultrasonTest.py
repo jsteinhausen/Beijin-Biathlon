ad2a2b47a09e7cf3a63eff2d46af58a7fcab2621
Brice <brice.lerousseau@gmail.com>
2022-05-18 16:52:57 +0200

    methode median ultrason test class retour try

--- ultrasonTest.py
+++ ultrasonTest.py
@@ -2,9 +2,9 @@ import RPi
 from ultrasound import Ultrasound
 
 sensor=Ultrasound(15, 16)
-#try:
-  #while True:
-print(sensor.median_dist())
-#except KeyboardInterrupt:
- # pass
-#RPi.GPIO.cleanup()
+try:
+
+ print(sensor.median_dist())
+except KeyboardInterrupt:
+  pass
+RPi.GPIO.cleanup()
