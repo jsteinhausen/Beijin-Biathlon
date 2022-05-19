53bd678a79fb4c197951495305e68a538d2b7daa
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-20 00:37:42 +0200

    main.py testing move_gun2angle fixed and prepared to test with 750

--- main.py
+++ main.py
@@ -158,7 +158,9 @@ def move_gun2angle(distance_z,distance_y):
     distance_y/=1000
     lam=(VELOCITY^2)/(G*distance_z)
     tanA=(distance_y-0.08)/distance_z
-    angle=math.atan((lam-(math.sqrt((pow(lam,2)-1-(2*lam*tanA))))))
+    a=pow(lam,2)-1-(2*lam*tanA)
+    b=lam-(math.sqrt(a))
+    angle=math.atan(b)
     if angle<=45:
         servo45.turn_to_angle(angle)
     else:
