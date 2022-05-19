aac3a7d6da552b41716ca56716461ebca39fd3c9
Brice <brice.lerousseau@gmail.com>
2022-05-19 11:30:47 +0200

    return 45

--- ServoTestClass.py
+++ ServoTestClass.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 import servo
 
-#servo1=servo.Servo(13,50)
-servo360=servo.Servo360(12,50)
-servo360.turn_to_angle(180)
+servo1=servo.Servo(13,50)
+#servo360=servo.Servo360(12,50)
+servo1.turn_to_angle(45)
 GPIO.cleanup()
 #servoPIN = 13
 
