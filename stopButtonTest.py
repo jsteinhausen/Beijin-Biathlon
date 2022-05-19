2d660f207409c9a99f899e8b66195b31b67e5ee8
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-19 14:13:17 +0200

    stopButtonTest.py even detect rising with bouncetime button feedback distance 2

--- stopButtonTest.py
+++ stopButtonTest.py
@@ -5,7 +5,7 @@ import adafruitMotorshield
 
 shield=adafruitMotorshield.AdafruitMotorShield()
 shield.createStepperMotor(1.8,65,0.1)
-shield.adafruitStepperMotor.stepperSpeed=0.1
+shield.adafruitStepperMotor.stepperSpeed=0.05
 #GPIO.setmode(GPIO.BCM)
 switch=6
 GPIO.setup(switch, GPIO.IN,GPIO.PUD_DOWN)
