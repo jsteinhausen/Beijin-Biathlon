4196f977e446095b89268040b5fb3239abc0142e
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-19 21:26:34 +0200

    shooting class muted

--- camera.py
+++ camera.py
@@ -3,10 +3,10 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 # Capture frame
-for i in range(3):
-	ret, frame = cap.read()
-	if ret:
-		cv2.imwrite('image'+str(i)+'.jpg', frame)
-		print("True")
 
-	cap.release()
+ret, frame = cap.read()
+if ret:
+	cv2.imwrite('image1.jpg', frame)
+	print("True")
+
+cap.release()
