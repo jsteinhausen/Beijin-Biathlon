e428a6c2c9f5b331eaf73c2144c89c3e17937907
Brice <brice.lerousseau@gmail.com>
2022-05-19 09:14:55 +0200

    retour servo pin 13

--- ServoTestClass.py
+++ ServoTestClass.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 import servo
 
-servo1=servo.Servo(12,50)
-servo1.turn_to_angle(180)
+servo1=servo.Servo(13,50)
+servo1.turn_to_angle(45)
 GPIO.cleanup()
 #servoPIN = 13
 
