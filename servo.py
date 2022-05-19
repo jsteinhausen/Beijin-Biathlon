bd169edbd113e540940f6cfbf48e2a17fb95a12c
Brice <brice.lerousseau@gmail.com>
2022-05-19 12:04:26 +0200

    p.start(2.0)

--- servo.py
+++ servo.py
@@ -6,7 +6,7 @@ class Servo:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(PIN, GPIO.OUT)
         self.p = GPIO.PWM(PIN, PWM)
-        self.p.start(2.5)
+        self.p.start(2.0)
 
     def translate(self,value, leftMin, leftMax, rightMin, rightMax):
         # Figure out how 'wide' each range is
