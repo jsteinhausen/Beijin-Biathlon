faa7f0d3b1d6d3e7fd6b889a49ce3c40005c12f2
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-19 13:22:55 +0200

    stopButtonTest.py try

---
+++ stopButtonTest.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+import adafruitMotorshield
+
+shield=adafruitMotorshield.AdafruitMotorShield()
+shield.createStepperMotor(1.8,65,0.1)
+shield.adafruitStepperMotor.stepperSpeed=0.005
+#GPIO.setmode(GPIO.BCM)
+switch=6
+GPIO.setup(switch, GPIO.IN,GPIO.PUD_DOWN)
+GPIO.add_event_detect(switch, GPIO.RISING)
+
+try:
+    while True:
+        shield.adafruitStepperMotor.oneStepForward()
+        print(GPIO.input(switch))
+        if GPIO.event_detected(switch):
+            print('Button pressed')
+            shield.adafruitStepperMotor.oneStepBackward()
+except KeyboardInterrupt:
+    pass
+GPIO.cleanup()
+print('stop')
