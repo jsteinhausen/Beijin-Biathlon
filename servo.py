6d6c551e658bc3186b203a190925fa6d16fb067f
Brice <brice.lerousseau@gmail.com>
2022-05-19 10:35:06 +0200

    nouvelle classe 360

--- servo.py
+++ servo.py
@@ -25,11 +25,31 @@ class Servo:
         self.p.ChangeDutyCycle(pwnPercent)
         #time.sleep(0.5)
         #return pwnPercent
-    def turn_to_angle_360(self,angle):
-        pwnPercent360 = self.translate(angle, 0.0, 180, 5.0, 10)
-        self.p.ChangeDutyCycle(10)
-        #time.sleep(0.5)
-        #return pwnPercent360
+
+class Servo360:
+        def __init__(self, PIN, PWM):
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(PIN, GPIO.OUT)
+            self.p = GPIO.PWM(PIN, PWM)
+            self.p.start(2.5)
+
+        def translate(self, value, leftMin, leftMax, rightMin, rightMax):
+            # Figure out how 'wide' each range is
+            leftSpan = leftMax - leftMin
+            rightSpan = rightMax - rightMin
+
+            # Convert the left range into a 0-1 range (float)
+            valueScaled = float(value - leftMin) / float(leftSpan)
+
+            # Convert the 0-1 range into a value in the right range.
+            return rightMin + (valueScaled * rightSpan)
+
+        # speed entre 5 et 10
+        def turn_to_angle(self, angle):
+            pwnPercent = self.translate(angle, 0.0, 180, 5.0, 10.0)
+            self.p.ChangeDutyCycle(pwnPercent)
+            # time.sleep(0.5)
+            return pwnPercent
 
 
 
