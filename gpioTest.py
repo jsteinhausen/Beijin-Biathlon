73d7c2f8a763be3dc471e359a241ec0dcae6b31d
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-19 13:09:42 +0200

    gpioTest.py try

--- gpioTest.py
+++ gpioTest.py
@@ -3,12 +3,14 @@ import time
 GPIO.setmode(GPIO.BCM)
 led=5
 GPIO.setup(led, GPIO.OUT,initial=GPIO.LOW)
-
-#LED ON
-GPIO.output(led, GPIO.HIGH)
-time.sleep(60)
-#LED OFF
-GPIO.output(led, GPIO.LOW)
-time.sleep(2)
+try:
+    #LED ON
+    GPIO.output(led, GPIO.HIGH)
+    time.sleep(60)
+    #LED OFF
+    GPIO.output(led, GPIO.LOW)
+    time.sleep(2)
+except KeyboardInterrupt:
+    pass
 GPIO.cleanup()
 
