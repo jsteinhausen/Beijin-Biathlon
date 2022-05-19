dcb87e3adde681f699ed42a89d0c8400ee86da8d
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-19 13:25:25 +0200

    stopButtonTest.py try

--- stopButtonTest.py
+++ stopButtonTest.py
@@ -5,7 +5,7 @@ import adafruitMotorshield
 
 shield=adafruitMotorshield.AdafruitMotorShield()
 shield.createStepperMotor(1.8,65,0.1)
-shield.adafruitStepperMotor.stepperSpeed=0.005
+shield.adafruitStepperMotor.stepperSpeed=0.05
 #GPIO.setmode(GPIO.BCM)
 switch=6
 GPIO.setup(switch, GPIO.IN,GPIO.PUD_DOWN)
@@ -20,5 +20,6 @@ try:
             shield.adafruitStepperMotor.oneStepBackward()
 except KeyboardInterrupt:
     pass
+shield.adafruitStepperMotor.stepperMotor.release()
 GPIO.cleanup()
 print('stop')
