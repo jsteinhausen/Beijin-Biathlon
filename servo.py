e751b7f3a9292578c81c9aa27ec7d73c20420276
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-20 04:01:09 +0200

    main.py servo.py

--- servo.py
+++ servo.py
@@ -45,7 +45,7 @@ class Servo360:
 
     #speed entre 5 et 10
     def turn_to_angle(self,angle):
-        self.p.start(2.0)
+        self.p.start(2.5)
         pwnPercent360 = self.translate(angle, 0.0, 180, 5.0, 10.0)
         self.p.ChangeDutyCycle(pwnPercent360)
         time.sleep(0.10)
