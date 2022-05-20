e0967d7e33831b3b6e66f0d68ce01543be3b754d
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2022-05-20 12:34:21 +0200

    servo.py stop in servo 45 start stop in same method

--- servo.py
+++ servo.py
@@ -5,7 +5,7 @@ class Servo45:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(PIN, GPIO.OUT)
         self.p = GPIO.PWM(PIN, PWM)
-        self.p.start(2.5)
+
 
     def translate(self,value, leftMin, leftMax, rightMin, rightMax):
         # Figure out how 'wide' each range is
@@ -20,9 +20,11 @@ class Servo45:
 
     #speed entre 5 et 10
     def turn_to_angle(self,angle):
+        self.p.start(2.5)
         pwnPercent = self.translate((45-angle), 0.0, 45, 5.0, 10)
         self.p.ChangeDutyCycle(pwnPercent)
         self.p.stop()
+
         #return pwnPercent
 
 class Servo360:
